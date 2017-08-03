9cb03ea87c088ed315d733d5f4c2b648d043cd8a
cfen <chris.fenn@guardian.co.uk>
2017-08-03 16:31:06 +0100

    many changes

--- src/render.js
+++ src/render.js
@@ -12,9 +12,9 @@ import { groupBy } from './js/libs/arrayObjectUtils.js'
 
 import Handlebars from 'handlebars'
 
-import countryCode from 'i18n-iso-countries'
-
 let maxMedal;
+
+let countries;
 
 let IAAFstem = "http://www.iaafworldchampionships.com/events/";
 
@@ -29,14 +29,14 @@ export async function render() {
     let medalsArr = getMedalsData(data)
     dataObj.medalsArr = medalsArr;
 
-    let daysArr = getDaysArr(data)
-    dataObj.daysArr = daysArr;
-
-    let recordsArr = getRecArr(data)
+    let recordsArr = getRecArr(data.records)
     dataObj.recordsArr = recordsArr;
 
     let countriesArr = getCountriesArr(medalsArr)
     dataObj.countriesArr = countriesArr;
+
+    let daysArr = getDaysArr(data);
+    dataObj.daysArr = daysArr;
 
     var compiledHTML = compileHTML(dataObj);
 
@@ -50,77 +50,41 @@ export async function loadData() {
         json: true
     }));
 
+
     return data;
 }
 
 
 function formatData(data) {
     var newObj = {};
-    let results = data.sheets.results;
+
+    countries = data.sheets.countries_list;
+
+    countries = getCountryNames(countries);
+
+
     let fixtures = data.sheets.fixtures;
-    let count = 0;
-    
 
-
-
-    results.map((obj) => {
-
-        fixtures.map((item) => {
-            item.sex = item.sex.toLowerCase();
-
-            //console.log(item.sex)
-            if(item.event_id == obj.event_id ){
-             
-                obj.sex = item.sex;
-                obj.date = item.date;
-                obj.start_time = item.start_time;              
-                obj.event = item.event;
-                obj.stage = item.stage;
-                obj.measure = item.measure;
-                
-                console.log(item)
-                //logs out measure
-            }
-            
-        })
-        
+    fixtures.map((obj) => {       
         obj.ref = count;
         obj.event = getShortEvent(obj.event);
         obj.athEvent = obj.sex + "_" + obj.event.split(" ").join("--") + "_" + obj.stage.split(" ").join("--");
-        obj.score = obj.result;
         obj.sex = obj.sex.toLowerCase();
-        if (obj.country == "United States") { obj.country = "United States of America" }
-        if (obj.country == "Great Britain") { obj.country = "United Kingdom" }
-        obj.ISO = countryCode.getAlpha3Code(obj.country, 'en');
-        obj.flag = obj.ISO.toLowerCase();
 
         count++;
     })
 
-    
+    let results = formatResultsData(data.sheets.results, fixtures);
 
-    count = 0;
+    let count = 0;
 
-    fixtures.map((obj) => {
-        obj.ref = count;
-        obj.event = getShortEvent(obj.event);
-        obj.athEvent = obj.sex + "_" + obj.event.split(" ").join("--") + "_" + obj.stage.split(" ").join("--");
-        count++;
-    })
-
-    let countries = data.sheets.countries_list;
-
-    countries = getCountryNames (countries);
-
-    let records = data.sheets.results;
-
-    records = records.filter(function(obj) {
+    let records = results.filter(function(obj) {
         return obj.record;
-    });
+    }); 
 
     records.map((obj) => {
         obj.event = getShortEvent(obj.event);
-        obj.athEvent = obj.sex + "_" + obj.event.split(" ").join("--");
+        obj.athEventRecord = obj.sex + "_" + obj.event.split(" ").join("--");
         obj.formatEvent = getFormatEvent(obj.sex, obj.event);
     })
 
@@ -134,59 +98,65 @@ function formatData(data) {
 }
 
 
+function formatResultsData(results, fixtures){
+
+    let count = 0;
+
+    results.map((res) => {
+
+        fixtures.map((item) => {
+            if(item.event_id == res.event_id ){
+                res.date = item.date;
+                res.start_time = item.start_time;              
+                res.event = item.event;
+                res.stage = item.stage;
+                res.measure = item.measure;
+            }
+
+            count ++
+        })
+        
+        res.ref = count;
+        res.event = getShortEvent(res.event);
+        
+        res.athEvent = res.sex + "_" + res.event.split(" ").join("--") + "_" + res.stage.split(" ").join("--");
+        res.score = res.result;
+
+        count++;
+    })
+
+    return results;
+}
+
 function getCountryNames(countries){
 
-    countries.map((obj) => {
-
-            if (obj.Country_Name == "United States") { obj.Country_Name = "United States of America" }
-            if (obj.Country_Name == "Great Britain" || obj.Country_Name == "Great Britain & N.I.") { obj.Country_Name = "United Kingdom" }
-            if (obj.Country_Name == "Antigua & Barbuda") { obj.Country_Name = "Antigua and Barbuda" }
-            if (obj.Country_Name == "Bosnia-Herzegovina") { obj.Country_Name = "Bosnia and Herzegovina" }
-            if (obj.Country_Name == "British Virgin Islands") { obj.Country_Name = "Virgin Islands, British" }
-            if (obj.Country_Name == "Brunei") { obj.Country_Name = "Brunei Darussalam" }
-            if (obj.Country_Name == "Cabo Verde") { obj.Country_Name = "Cape Verde" }
-            if (obj.Country_Name == "Chinese Taipei") { obj.Country_Name = "Taiwan" }
-            if (obj.Country_Name == "Commonwealth Of Dominica") { obj.Country_Name = "Dominican Republic" }
-            if (obj.Country_Name == "Dem. Rep. Of Sao Tome And Principe") { obj.Country_Name = "Sao Tome and Principe" }
-            if (obj.Country_Name == "Democ. Republic Of Congo") { obj.Country_Name = "Congo, the Democratic Republic of the" }
-            if (obj.Country_Name == "Dpr Of Korea") { obj.Country_Name = "Korea, Democratic People's Republic of" }
-            if (obj.Country_Name == "East Timor") { obj.Country_Name = "Timor-Leste" }
-            if (obj.Country_Name == "F Y Rep. Of Macedonia") { obj.Country_Name = "Macedonia, the Former Yugoslav Republic of" }
-            if (obj.Country_Name == "Hong Kong, China") { obj.Country_Name = "Hong Kong" }
-            if (obj.Country_Name == "Islamic Republic Of Iran") { obj.Country_Name = "Iran, Islamic Republic of" }
-            if (obj.Country_Name == "Kirghizistan") { obj.Country_Name = "Kyrgyzstan" }
-            if (obj.Country_Name == "Kiribati Rep Of") { obj.Country_Name = "Kiribati" }
-            if (obj.Country_Name == "Korea") { obj.Country_Name = "Korea, Republic of" }
-            if (obj.Country_Name == "Laos") { obj.Country_Name = "Lao People's Democratic Republic" }
-            if (obj.Country_Name == "Micronesia") { obj.Country_Name = "Micronesia, Federated States of" }
-            if (obj.Country_Name == "Moldova") { obj.Country_Name = "Moldova, Republic of" }
-            if (obj.Country_Name == "Palestine") { obj.Country_Name = "Palestinian Territory, Occupied" }
-            if (obj.Country_Name == "Pr Of China") { obj.Country_Name = "China" }
-            if (obj.Country_Name == "Rep Of Nauru - Pacific") { obj.Country_Name = "Nauru" }
-            if (obj.Country_Name == "Rep Of Palau - Pacific") { obj.Country_Name = "Palau" }
-            if (obj.Country_Name == "Republic Of Yemen") { obj.Country_Name = "Yemen" }
-            if (obj.Country_Name == "Russia") { obj.Country_Name = "Russian Federation" }
-            if (obj.Country_Name == "Saint Vincent") { obj.Country_Name = "Saint Vincent and the Grenadines" }
-            if (obj.Country_Name == "Slovak Republic") { obj.Country_Name = "Slovakia" }
-            if (obj.Country_Name == "Surinam") { obj.Country_Name = "Suriname" }
-            if (obj.Country_Name == "Syria") { obj.Country_Name = "Syrian Arab Republic" }
-            if (obj.Country_Name == "Tanzania") { obj.Country_Name = "Tanzania, United Republic of" }
-            if (obj.Country_Name == "The Gambia") { obj.Country_Name = "Gambia" }
-            if (obj.Country_Name == "Vietnam") { obj.Country_Name = "Viet Nam" }
-            if (obj.Country_Name == "Virgin Islands") { obj.Country_Name = "Virgin Islands, U.S." }
-            if (obj.Country_Name == "Saint Vincent and the Grenadines") { obj.ISO = "VCT" } else { obj.ISO = countryCode.getAlpha3Code(obj.Country_Name, 'en'); }
-
-            obj.flag = obj.ISO.toLowerCase();
-
+    countries.map((obj) => {  
+        obj.country = obj.Country_Name;
+        obj.ISO = obj.iso;
+        obj.flag = obj.ISO.toLowerCase(); 
     })
 
     return countries;
 }
 
 
+function getISO(o){
+    let c = o.country;
+    countries.map((obj) => {
+        if(c === obj.country){
+            o.ISO = obj.ISO;
+            o.flag = obj.flag;
+        }
+    })
+
+    return o;
+
+};
+
 function getMedalsData(data) {
 
     let a = groupBy(data.countries, "ISO");
+
     a = sortByKeys(a);
 
     a.map((obj) => {
@@ -198,6 +168,7 @@ function getMedalsData(data) {
         obj.medallists = [];
 
         data.results.map((item) => {
+
             if (item.medal === "Gold" && item.country === obj.objArr[0].Country_Name) { obj.medal.gold++ }
             if (item.medal === "Silver" && item.country === obj.objArr[0].Country_Name) { obj.medal.silver++ }
             if (item.medal === "Bronze" && item.country === obj.objArr[0].Country_Name) { obj.medal.bronze++ }
@@ -205,7 +176,8 @@ function getMedalsData(data) {
             item.formatDate = getFormatDate(item.date, "DD Mmm");
             item.formatEvent = getFormatEvent(item.sex, item.event);
             
-            item.sex == "w" ? item.sexStr = "Women’s " : item.sexStr = "Men’s ";
+            if (item.sex == "w"){item.sexStr = "Women’s "}  
+            if (item.sex == "m"){item.sexStr = "Men’s " };
 
             if (item.medal && item.country === obj.objArr[0].Country_Name) { obj.medallists.push(item) }
         })
@@ -234,8 +206,8 @@ function getMedalsData(data) {
     })
 
     return a;
-}
 
+}
 
 
 
@@ -272,9 +244,9 @@ function getEventsData(data) {
         obj.gender = obj.objArr[0].sex.toLowerCase();
         obj.stage = obj.objArr[0].stage;
         obj.iaaf_url = IAAFstem+obj.objArr[0].event_id ;
-       
         obj.gender == "w" ? obj.gender = "Women’s" : obj.gender = "Men’s";
         obj.formatTitle = obj.objArr[0].event + " " + obj.objArr[0].stage;
+
 
         if (obj.objArr[0].stage == "Final") { obj.highlightEvent = true };
         obj.objArr[0].result && obj.stage == "Final" ? obj.result = true : obj.result = false;
@@ -286,6 +258,7 @@ function getEventsData(data) {
 
         obj.objArr.map((athlete, k) => {
             athlete.place = k + 1;
+           
         })
     })
 
@@ -295,21 +268,20 @@ function getEventsData(data) {
 }
 
 function getItemResults(item, results) {
-
+   
     let t = item.athEvent.toLowerCase();
-
-
-
+    let s = item.sex.toLowerCase();
+    
     var tempArr = [];
-    results.map((obj, k) => {
+    results.map((obj) => {
+
+        obj.sex = s;
         obj.measure = item.measure;
-        
-        if (t === obj.athEvent.toLowerCase()) {
-            
-            
+        if( t == obj.athEvent.toLowerCase()){
             tempArr.push(obj);
-        }
+        }  
     })
+
     tempArr.sort((a, b) => (a.score - b.score))
 
     return tempArr;
@@ -317,36 +289,35 @@ function getItemResults(item, results) {
 
 
 function getRecArr(data) {
+
     let a = []
 
     let obj = {};
     obj.objKey = "Records";
-    obj.objArr = data.results;
-
-    obj.objArr = obj.objArr.filter(function(obj) {
-        return obj.record;
-    });
+    obj.objArr = data;
 
     obj.objArr.map((item, k) => {
-
         item.formatDate = getFormatDate(item.date, "Mmm YYYY");
         item.date = new Date(item.date);
         item.formatEvent = getFormatEvent("null", item.event);
 
         item.sex == "w" ? item.sexStr = "Women’s " : item.sexStr = "Men’s ";
 
-        if (item.country == "United States") { item.country = "United States of America" }
-        if (item.country == "Great Britain") { item.country = "United Kingdom" }
+   
         if (item.record == "national") { item.nationalRecord = true }
         if (item.record == "world") { item.worldRecord = true }
         if (item.record == "games") { item.gamesRecord = true }
 
-        item.ISO = countryCode.getAlpha3Code(item.country, 'en');
+
 
         if (item.country == "East Germany") { item.ISO = "DDR" }
         if (item.country == "Czechoslovakia") { item.ISO = "TCH" }
 
-        item.flag = item.ISO.toLowerCase();
+        console.log(item.country)    
+
+        item = getISO(item);
+
+        
     })
 
     obj.objArr.sort((a, b) => (a.date - b.date))
@@ -372,15 +343,8 @@ function getOneDArr(data, series) {
 
         item.sex == "w" ? item.sexStr = "Women’s " : item.sexStr = "Men’s ";
 
-        if (item.country == "United States") { item.country = "United States of America" }
-        if (item.country == "Great Britain") { item.country = "United Kingdom" }
+        item = getISO(item);
 
-        item.ISO = countryCode.getAlpha3Code(item.country, 'en');
-
-        if (item.country == "East Germany") { item.ISO = "DDR" }
-        if (item.country == "Czechoslovakia") { item.ISO = "TCH" }
-
-        item.flag = item.ISO.toLowerCase();
 
     })
     obj.objArr.sort((a, b) => (a.date - b.date))
@@ -405,10 +369,6 @@ function getRecordsArr(data, series) {
 
             item.sex == "w" ? item.sexStr = "Women’s " : item.sexStr = "Men’s ";
 
-            if (item.country == "United States") { item.country = "United States of America" }
-            if (item.country == "Great Britain") { item.country = "United Kingdom" }
-
-            item.ISO = countryCode.getAlpha3Code(item.country, 'en');
 
             if (item.country == "East Germany") { item.ISO = "DDR" }
             if (item.country == "Czechoslovakia") { item.ISO = "TCH" }
@@ -428,6 +388,7 @@ function getRecordsArr(data, series) {
 
 
 function getDaysArr(data) {
+
     let a = groupBy(data.fixtures, "date");
     a = sortByKeys(a);
 
@@ -438,23 +399,24 @@ function getDaysArr(data) {
 
         obj.objArr.map((item, k) => {
 
-
-
             if (item.result) {
                 item.resultTable = getItemResults(item, data.results);
-                //item.measure = item.resultTable[0].measure;
-                //error here
-
+                
                 item.measure == "time" ? item.resultTable.sort((a, b) => (a.score - b.score)) : item.resultTable.sort((a, b) => (b.score - a.score));
 
                 item.resultTable.map((result, k) => {
+
+                    //result.sex = item.sex.toLowerCase()
                     result.place = k;
                 })
             }
+
             item.date = new Date(item.date + " " + item.start_time);
             item.sortTime = item.start_time.split(":").join("");
-            //logs out measure
+
         })
+
+
     })
 
     a.sort((a, b) => (a.date - b.date))
